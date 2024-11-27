1f6ed250f144074b73674cdd3be8286e7015528e
Neyyala Haritha <harithaneyyala@gmail.com>
2024-11-27 09:41:10 +0530

    task

---
+++ task-1/index.js
@@ -0,0 +1,38 @@
+// task-1 write a loop to print numbers from 1 to 20 
+ 
+// for(i=1;i<=20;i++){
+//     console.log(i);
+// }
+
+// task-2 Create a for loop to print the squares of numbers from 1 to 10.
+// for(i=1;i<=10;i++){
+//     console.log(`square of ${i} is: ${i*i}`);
+// }
+
+// Use a for loop to print the first 10 multiples of 5.
+// for(i=1;i<=10;i++){
+//     console.log(`5 * ${i} is: ${i*5}`);
+// }
+
+// task-4 Write a for loop to print numbers from 20 to 1 in reverse order.
+// for(i=20;i>=1;i--){
+//     console.log(i);
+// }
+
+// task-5 5.Create a for loop to print the first 10 even numbers in reverse order (e.g., 20, 18, 16, …)
+
+// for(i=20;i>=2;i-=2){
+//     console.log(i);
+// }
+
+// task-6 6.Print each element of the array Input: [10, 20, 30, 40, 50]
+// var array=[10,20,20,40,50];
+// for(i=0;i < array.length;i++){
+//     console.log(array[i] + " ");
+// }
+
+// task-7 7.Print the index of each element along with its value –Input: ['apple', 'banana', 'cherry']
+var fruits=['apple', 'banana', 'cherry'];
+for(i=0;i<fruits.length;i++){
+    console.log(`Index ${i}: ${fruits[i]}`)
+}
